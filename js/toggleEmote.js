0b95f90e8a9bd7b82e5d0804e1d9859da978c721
Patrickgh3 <patrickjtraynor@gmail.com>
2020-09-20 00:40:11 -0700

    More work

--- js/toggleEmote.js
+++
@@ -1,9 +0,0 @@
-// Toggle image source between yoyoYo and cornBlack
-function toggleEmote(imgElt) {
-    if (imgElt.src.search("yoyoYo.png") != -1) {
-        imgElt.src = "images/cornBlack.png";
-    }
-    else {
-        imgElt.src = "images/yoyoYo.png";
-    }
-}
